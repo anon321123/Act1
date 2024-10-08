7b795d8b28444a93d10cfe91a96c27120b542dbd
anon321123 <joselorenzotuazon321@gmail.com>
2024-10-08 20:43:37 +0800

    attemps

---
+++ app/Views/landing.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial scale=1.0">
+    <title>Mima Catlogs</title>
+</head>
+<body>
+    <header class="header">
+        <img src="" alt="Mima" class="logo">
+        <nav class="nav">
+        <a href="" class="btn">Home</a>
+        <a href="" class="btn">catalogue</a>
+        <a href="" class="btn">about</a>
+    </nav>
+        <a href="" class="btn action"></a>
+    </header>
+</body>
+</html>
